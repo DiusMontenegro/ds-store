2c9fc70802fee65bc5d01a9cf2892e69b1f005bc
Dius Montenegro <montenegrodiussantos1@gmail.com>
2024-11-23 11:51:44 +0800

    style: Homepage, Featured products, Product card and Product skeleton

---
+++ components/shared/Product.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { products } from "@wix/stores";
+import Link from "next/link";
+import { media as wixMedia } from "@wix/sdk";
+
+interface ProductProps {
+    product: products.Product;
+}
+
+const Product = ({ product }: ProductProps) => {
+    const mainImage = product.media?.mainMedia?.image;
+    const resizedImageUrl = mainImage?.url ? wixMedia.getScaledToFillImageUrl(mainImage.url, 700, 700, {}) : null;
+
+    return (
+        <Link href={`/products/${product.slug}`} className="h-full border">
+            <div className="overflow-hidden">
+                <img
+                    src={resizedImageUrl || "/assets/image-placeholder.jpg"}
+                    alt={mainImage?.altText || "image-placeholder"}
+                    className="transition-transform duration-300 hover:scale-105"
+                />
+            </div>
+            <div className="space-y-3 p-3">
+                <h3 className="text-lg font-bold">{product.name}</h3>
+                <div className="line-clamp-5" dangerouslySetInnerHTML={{ __html: product.description || "" }} />
+            </div>
+        </Link>
+    );
+};
+
+export default Product;
