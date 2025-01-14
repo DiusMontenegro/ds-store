e24cf297ec69a7d92ce84cb7ff694d20762ac30e
Dius Montenegro <montenegrodiussantos1@gmail.com>
2025-01-14 21:06:56 +0800

    feat: separate wixImage resizer/scaler in a separate reusable component with optional width and height props

--- components/shared/Product.tsx
+++ components/shared/Product.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { products } from "@wix/stores";
 import Link from "next/link";
-import { media as wixMedia } from "@wix/sdk";
+import WixImage from "./WixImage";
+import Badge from "../ui/badge";
+import { getFormattedPrice } from "@/lib/utils";
 
 interface ProductProps {
     product: products.Product;
@@ -9,16 +11,21 @@ interface ProductProps {
 
 const Product = ({ product }: ProductProps) => {
     const mainImage = product.media?.mainMedia?.image;
-    const resizedImageUrl = mainImage?.url ? wixMedia.getScaledToFillImageUrl(mainImage.url, 700, 700, {}) : null;
 
     return (
         <Link href={`/products/${product.slug}`} className="h-full border">
-            <div className="overflow-hidden">
-                <img
-                    src={resizedImageUrl || "/assets/image-placeholder.jpg"}
-                    alt={mainImage?.altText || "image-placeholder"}
+            <div className="relative overflow-hidden">
+                <WixImage
+                    mediaIdentifier={mainImage?.url}
+                    alt={mainImage?.altText}
+                    width={700}
+                    height={700}
                     className="transition-transform duration-300 hover:scale-105"
                 />
+                <div className="absolute bottom-3 right-3 flex flex-wrap items-center gap-2">
+                    {product.ribbon && <Badge>{product.ribbon}</Badge>}
+                    <Badge>{getFormattedPrice(product)}</Badge>
+                </div>
             </div>
             <div className="space-y-3 p-3">
                 <h3 className="text-lg font-bold">{product.name}</h3>
